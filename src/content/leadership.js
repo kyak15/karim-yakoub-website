c85cdde46c6e12ae36df9050364d4766de245911
karim <karimyakoub1515@gmail.com>
2024-11-04 22:58:44 -0500

    fixing writing

--- src/content/leadership.js
+++ src/content/leadership.js
@@ -5,24 +5,20 @@ const leadershipContent =
             "company": "Ramapo College Office of Residence Life",
             "location": "Mahwah, NJ",
             "duration": "Aug 2019 - May 2022",
-            "extra": "teachfirstbyte.org",
+            "extra": "",
             "roles": [
 
                 {
                     "title": "Resident Assistant",
                     "description": [
-                        "At Ramapo College, I embraced the role of a leader and mentor within the college community, where I was instrumental in developing over 15 programs aimed at fostering strong community bonds. My responsibilities ranged from performing administrative tasks to providing critical support during crisis situations on duty nights. This role honed my abilities to collaborate effectively with diverse groups and build inclusive environments, underscoring my commitment to community building and teamwork in every project I undertake.",
-                        "In my position at Ramapo College, I significantly enhanced my organizational skills and ability to collaborate within large teams, including working closely with college directors. I took the lead in orchestrating community-building programs that brought together diverse groups, fostering a sense of unity and cooperation. This role not only sharpened my project management abilities but also deepened my expertise in creating and nurturing a vibrant community, demonstrating my capacity to innovate and lead in dynamic environments."
-
-
-                        
-                        
+                        "At Ramapo College, I embraced the role of a leader within the college community, where I developed over 15 programs aimed at developing community within my residence hall. My responsibilities ranged from performing administrative tasks to providing critical support during crisis situations on duty nights. This role strengthened my abilities to collaborate effectively with diverse groups and build inclusive environments.",
+                        "In my position at Ramapo College, I significantly enhanced my organizational skills and ability to collaborate within large teams, including working closely with college directors. I took the lead in orchestrating community-building programs that brought together diverse groups, fostering a sense of unity and cooperation. This role not only sharpened my project management abilities but also deepened my expertise in creating and nurturing a vibrant community, demonstrating my capacity to innovate and lead in dynamic environments."                        
                     ],
                 },
                 {
                     "title": "Student Assistant Resident Director",
                     "description": [
-                        "During a critical shortage of leadership at Ramapo College amidst the COVID-19 pandemic, I stepped up as a Senior Assistant Resident Director, leading a team of nine Resident Assistants. This role required close collaboration with college directors and honed my leadership skills under pressure. I navigated the team through the complexities of pandemic management, emphasizing safety, support, and community resilience. My tenure underscored my ability to lead decisively and compassionately in times of significant stress and change."
+                        "Between January 2021 - May 2021, I was chosen to work as a Senior Assistant Resident Director, leading a team of nine Resident Assistants. This role required close collaboration with college directors and improved my leadership skills under pressure. I navigated the team through the tasks of pandemic management, emphasizing safety and support. My time as a SARD exemplified my ability to lead decisively and compassionately in times of stress and change."
                     ],
                 }
             ],
@@ -43,10 +39,7 @@ const leadershipContent =
                 }
             ],
             "skills": [
-                { "text": "Project Management" },
-                { "text": "Swift 4  " },
-                { "text": "Xcode" },
-                { "text": "MVC Architecture" },
+   
             ]
         },
 
