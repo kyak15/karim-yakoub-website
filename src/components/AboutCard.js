3c65a4ae6ddc31853a9436d79813dbd78264f5e4
karim <karimyakoub1515@gmail.com>
2024-11-05 00:28:24 -0500

    fixing download function

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -24,7 +24,7 @@ const AboutCard = ({ darkMode }) => {
     const downloadCV = async()=>{
         try {
             const URL = `https://www.googleapis.com/drive/v3/files/${process.env.REACT_APP_FILE_ID}/export?key=${process.env.REACT_APP_API_KEY}&alt=media`
-            const response = await axios.get(URL, {responseType: 'blob'})
+            const response = await axios.get(URL, {responseType: 'blob', params: {'mimeType': 'application/pdf' }})
             download(response.data, 'Karim_Yakoub_Resume.pdf', 'application/pdf')
             
         } catch (error) {
@@ -78,7 +78,7 @@ const AboutCard = ({ darkMode }) => {
                 <div className='text-center mt-4'>
                     <Card.Link
                         onClick={downloadCV}
-                        className={`btn btn-primary btn-sm round ${darkMode ? 'btn-light text-light':'btn-dark text-dark'}`}
+                        className={`btn btn-primary btn-sm round ${darkMode ? 'btn-light text-dark':'btn-dark text-light'}`}
                         target='blank'
                     >
                         
