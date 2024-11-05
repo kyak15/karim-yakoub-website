b666e0275be3f5c15c7f811710269370e3b60cb6
karim <karimyakoub1515@gmail.com>
2024-11-05 00:02:37 -0500

    fixing env vars

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -22,11 +22,11 @@ const AboutCard = ({ darkMode }) => {
     
 
     const downloadCV = async()=>{
-        const URL = `googleapis.com/drive/v3/files/${process.env.REACT_APP_FILE_ID}/export?key=${process.env.REACT_APP_API_KEY}`
-        const response = await axios.get(URL, {responseType: 'blobm', params: {'mimetype': 'application/pdf' }})
+        const URL = `https://www.googleapis.com/drive/v3/files/${process.env.REACT_APP_FILE_ID}/export?key=${process.env.REACT_APP_API_KEY}`
+        const response = await axios.get(URL, {responseType: 'blob', params: {'mimetype': 'application/pdf' }})
 
         try {
-            download(response.data, 'Karim_Yakoub_Resume.pdf', 'application/pdf')
+            await download(response.data, 'Karim_Yakoub_Resume.pdf', 'application/pdf')
             
         } catch (error) {
             alert('Sorry! Issue Downloading Resume')
